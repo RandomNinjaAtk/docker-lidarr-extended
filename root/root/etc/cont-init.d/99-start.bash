b219534407088a6c72090f799298ea01d0aacb4f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-06-11 20:39:13 -0400

    Create 99-start.bash

---
+++ root/root/etc/cont-init.d/99-start.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bash
+echo "------------------------------------------------------------"
+echo "|~) _ ._  _| _ ._ _ |\ |o._  o _ |~|_|_|"
+echo "|~\(_|| |(_|(_)| | || \||| |_|(_||~| | |<"
+echo "Presenets: lidarr-extended"
+echo "------------------------------------------------------------"
+echo "Donate: https://github.com/sponsors/RandomNinjaAtk"
+echo "Project: https://github.com/RandomNinjaAtk/docker-lidarr-extended"
+echo "Support: https://discord.gg/JumQXDc"
+echo "------------------------------------------------------------"
+
+if [ "$autoStart" = "true" ]; then
+	echo "Automatic Start Enabled, starting..."
+	bash /scripts/start.sh
+else
+	echo "Automatic Start Disabled, manually run using this command:"
+	echo "bash /scripts/start.sh"
+fi
+
+exit $?
