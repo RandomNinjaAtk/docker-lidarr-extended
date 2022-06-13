ea5a92e6944223256ee92d1614c63098d1fd1f03
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-06-13 20:27:08 +0000

    Update start.sh

--- root/scripts/start.sh
+++ root/scripts/start.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/with-contenv bash
 
 echo "Starting Script...."
-processstartid="$(ps -A -o pid,cmd|grep "/config/scripts/start.bash" | grep -v grep | head -n 1 | awk '{print $1}')"
+processstartid="$(ps -A -o pid,cmd|grep "/scripts/start.bash" | grep -v grep | head -n 1 | awk '{print $1}')"
 echo "To kill script, use the following command:"
 echo "kill -9 $processstartid"
 for (( ; ; )); do
 	let i++
-	bash /scripts/download.sh 2>&1 | tee "/config/logs/script_run_${i}_$(date +"%Y_%m_%d_%I_%M_%p").log" > /proc/1/fd/1 2>/proc/1/fd/2
-	if [ ! -d "/config/logs" ]; then
-		mkdir -p "/config/logs" 
-		chmod 777 "/config/logs" 
-		chown abc:abc "/config/logs" 
+	if [ ! -d "/config/extended/logs" ]; then
+		mkdir -p "/config/extended/logs" 
+		chmod 777 "/config/extended/logs" 
+		chown abc:abc "/config/extended/logs" 
 	fi
-	if [ -f "/config/logs/log-cleanup" ]; then
-		rm "/config/logs/log-cleanup"
+	bash /scripts/download.sh 2>&1 | tee "/config/extended/logs/script_run_${i}_$(date +"%Y_%m_%d_%I_%M_%p").log" > /proc/1/fd/1 2>/proc/1/fd/2
+	if [ -f "/config/extended/logs/log-cleanup" ]; then
+		rm "/config/extended/logs/log-cleanup"
 	fi
-	touch -d "8 hours ago" "/config/logs/log-cleanup"
-	if find "/config/logs" -type f -iname "*.log" -not -newer "/config/logs/log-cleanup" | read; then
-		find "/config/logs" -type f -iname "*.log" -not -newer "/config/logs/log-cleanup" -delete
+	touch -d "8 hours ago" "/config/extended/logs/log-cleanup"
+	if find "/config/extended/logs" -type f -iname "*.log" -not -newer "/config/extended/logs/log-cleanup" | read; then
+		find "/config/extended/logs" -type f -iname "*.log" -not -newer "/config/extended/logs/log-cleanup" -delete
 	fi
-	if [ -f "/config/logs/log-cleanup" ]; then
-		rm "/config/logs/log-cleanup"
+	if [ -f "/config/extended/logs/log-cleanup" ]; then
+		rm "/config/extended/logs/log-cleanup"
 	fi
 	if [ -z "$scriptInterval" ]; then
 		scriptInterval="15m"
 	fi
+	echo "Script sleeping for $scriptInterval..."
 	sleep $scriptInterval
 done
 
