c98ec5043ed4714a72fb73a5260516441671191f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-06-14 07:13:23 -0400

    Update 98-script-setup.bash

--- root/etc/cont-init.d/98-script-setup.bash
+++ root/etc/cont-init.d/98-script-setup.bash
@@ -1,36 +1,36 @@
 #!/usr/bin/with-contenv bash
 
 # create scripts directory if missing
-if [ ! -d "/config/lidarr-extended/scripts" ]; then
-	mkdir -p "/config/lidarr-extended/scripts"
+if [ ! -d "/config/extended/scripts" ]; then
+	mkdir -p "/config/extended/scripts"
 else
 	echo "Updating scripts..."
-	rm -rf /config/lidarr-extended/scripts/*
+	rm -rf /config/extended/scripts/*
 fi
 
 if [ -d "/config/lidarr-extended/scripts" ]; then
-	cp -r /scripts/* /config/lidarr-extended/scripts/
+	cp -r /scripts/* /config/extended/scripts/
 fi
 
 # create cache directory if missing
-if [ ! -d "/config/lidarr-extended/cache" ]; then
-	mkdir -p "/config/lidarr-extended/cache"
+if [ ! -d "/config/extended/cache" ]; then
+	mkdir -p "/config/extended/cache"
 fi
 
 # create logs directory if missing
-if [ ! -d "/config/lidarr-extended/logs" ]; then
-	mkdir -p "/config/lidarr-extended/logs"
+if [ ! -d "/config/extended/logs" ]; then
+	mkdir -p "/config/extended/logs"
 fi
 
 
 # set permissions
-chmod 777 /config/lidarr-extended
-chmod 777 /config/lidarr-extended/cache
-chmod 777 /config/lidarr-extended/logs
-chmod 666 /config/lidarr-extended/cache/*
-chmod 666 /config/lidarr-extended/logs/*
-chmod -R 777 /config/lidarr-extended/scripts
-chown -R abc:abc /config/lidarr-extended
+chmod 777 /config/extended
+chmod 777 /config/extended/cache
+chmod 777 /config/extended/logs
+chmod 666 /config/extended/cache/*
+chmod 666 /config/extended/logs/*
+chmod -R 777 /config/extended/scripts
+chown -R abc:abc /config/extended
 
 
 echo "Complete..."
