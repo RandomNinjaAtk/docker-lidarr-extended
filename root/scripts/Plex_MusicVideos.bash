4a53bdf52449c9713f6b159047e5549aa5f389b0
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-06-20 20:31:18 -0400

    Update Plex_MusicVideos.bash

--- root/scripts/Plex_MusicVideos.bash
+++ root/scripts/Plex_MusicVideos.bash
@@ -31,13 +31,16 @@ lidarrArtistData="$(curl -s "$lidarrUrl/api/v1/artist/$lidarrArtistId?apikey=${l
 lidarrArtistPath="$(echo "${lidarrArtistData}" | jq -r " .path")"
 tidalArtistUrl=$(echo "${lidarrArtistData}" | jq -r ".links | .[] | select(.name==\"tidal\") | .url")
 tidalArtistId="$(echo "$tidalArtistUrl" | grep -o '[[:digit:]]*' | sort -u)"
-#echo "$lidarrArtistData"
-echo "$tidalArtistUrl"
-echo "$tidalArtistId"
-skipTidal=false
+
+if [ "$dlClientSource" = "tidal" ] || [ "$dlClientSource" = "both" ]; then
+	skipTidal=false
+else
+	skipTidal=true
+fi
+
 if [ "$skipTidal" = "false" ]; then
 	if [ -z "$tidalArtistUrl" ]; then 
-		log ":: TIDAL :: ERROR :: musicbrainz id: $lidarrArtistForeignArtistId is missing Tidal link, see: \"/config/logs/tidal-arist-id-not-found.txt\" for more detail..."
+		log "TIDAL :: ERROR :: musicbrainz id: $lidarrArtistForeignArtistId is missing Tidal link, see: \"/config/logs/tidal-arist-id-not-found.txt\" for more detail..."
 		touch "/config/logs/tidal-arist-id-not-found.txt" 
 		if cat "/config/logs/tidal-arist-id-not-found.txt" | grep "https://musicbrainz.org/artist/$lidarrArtistForeignArtistId/relationships" | read; then
 			sleep 0.01
@@ -51,58 +54,59 @@ if [ "$skipTidal" = "false" ]; then
 fi
 
 if [ "$skipTidal" = "false" ]; then
-	echo "house"
 	if [ ! -d /config/extended/cache/tidal ]; then
 		mkdir -p /config/extended/cache/tidal
 	fi
 						
 	if [ ! -f /config/extended/cache/tidal/$tidalArtistId-videos.json ]; then
+		log "Caching Artist Videos List"
 		curl -s "https://api.tidal.com/v1/artists/${tidalArtistId}/videos?limit=10000&countryCode=$CountryCode&filter=ALL" -H 'x-tidal-token: CzET4vdadNUFQ5JU' > /config/extended/cache/tidal/$tidalArtistId-videos.json
 	fi
 
 fi
-echo "test"
+
 if [ "$skipTidal" = "false" ]; then
-	for i in $(cat extended/cache/tidal/$tidalArtistId-videos.json | jq -r '.items | sort_by(.duration) | .[].id'); do
-		tidalVideoTitle=$(cat extended/cache/tidal/$tidalArtistId-videos.json | jq -r ".items[] | select(.id==$i) | .title")
-		if find "$lidarrArtistPath" -type f -iname "* - $tidalVideoTitle.*" | read; then
-			echo "$i :: $tidalVideoTitle :: Match"
-			matchedFile="$(find "$lidarrArtistPath" -type f -iname "* - $tidalVideoTitle.*")"
-			echo "$matchedFile"
-			fileDirectory="$(dirname "$matchedFile")"
-			fileName="$(basename "$matchedFile")"
-			fileNameNoExt="${fileName%.*}"
+	tidalArtistVideoCount=$(cat /config/extended/cache/tidal/$tidalArtistId-videos.json | jq -r '.items | sort_by(.duration) | .[].id' | wc -l)
+	if [ $tidalArtistVideoCount -ge 1 ]; then
+		for i in $(cat /config/extended/cache/tidal/$tidalArtistId-videos.json | jq -r '.items | sort_by(.duration) | .[].id'); do
+			tidalVideoTitle=$(cat e/config/xtended/cache/tidal/$tidalArtistId-videos.json | jq -r ".items[] | select(.id==$i) | .title")
+			if find "$lidarrArtistPath" -type f -iname "* - $tidalVideoTitle.*" | read; then
+				echo "$i :: $tidalVideoTitle :: Match"
+				matchedFile="$(find "$lidarrArtistPath" -type f -iname "* - $tidalVideoTitle.*")"
+				echo "$matchedFile"
+				fileDirectory="$(dirname "$matchedFile")"
+				fileName="$(basename "$matchedFile")"
+				fileNameNoExt="${fileName%.*}"
 
+				if [ ! -f "$fileDirectory/$fileNameNoExt.mkv" ]; then
+					if [ ! -d /downloads/lidarr-extended/music-videos ]; then
+						mkdir -p /downloads/lidarr-extended/music-videos
+						chmod 777 /downloads/lidarr-extended/music-videos
+						chown abc:abc /downloads/lidarr-extended/music-videos
+					else
+						rm -rf /downloads/lidarr-extended/music-videos/*
+					fi
+					
+					tidal-dl -r P1080
+					tidal-dl -o /downloads/lidarr-extended/music-videos -l "https://tidal.com/browse/video/$i"
 
-			echo $fileDirectory
-			echo $fileName
-			echo $fileNameNoExt
-			if [ ! -f "$fileDirectory/$fileNameNoExt.mkv" ]; then
-				if [ ! -d /downloads/lidarr-extended/music-videos ]; then
-					mkdir -p /downloads/lidarr-extended/music-videos
-					chmod 777 /downloads/lidarr-extended/music-videos
-					chown abc:abc /downloads/lidarr-extended/music-videos
-				else
+					find "/downloads/lidarr-extended/music-videos" -type f -iname "*.mp4" -print0 | while IFS= read -r -d '' video; do
+						ffmpeg \
+						-i "${video}" \
+						-vcodec copy \
+						-acodec copy \
+						"$fileDirectory/$fileNameNoExt.mkv"
+					done
+					log "Downloaded Video to: $fileDirectory/$fileNameNoExt.mkv"
 					rm -rf /downloads/lidarr-extended/music-videos/*
 				fi
 
-    		tidal-dl -r P1080
-				tidal-dl -o /downloads/lidarr-extended/music-videos -l "https://tidal.com/browse/video/$i"
 
-				find "/downloads/lidarr-extended/music-videos" -type f -iname "*.mp4" -print0 | while IFS= read -r -d '' video; do
-					ffmpeg \
-					-i "${video}" \
-					-vcodec copy \
-					-acodec copy \
-					"$fileDirectory/$fileNameNoExt.mkv"
-				done
-				echo "Downloaded Video to: $fileDirectory/$fileNameNoExt.mkv"
-				rm -rf /downloads/lidarr-extended/music-videos/*
 			fi
-
-			
-		fi
-	done
+		done
+	else
+		log "ERROR :: No videos found for the artist on Tidal"
+	fi
 fi
 
 exit 0
