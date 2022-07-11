c2a7cff725508ed655d9894f55778fedd7531082
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-07-11 11:12:43 +0000

    v1.0.002 - Extract FLAC synced Lyrics to lrc file for media players

--- root/scripts/MetadataPostProcess.bash
+++ root/scripts/MetadataPostProcess.bash
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-version=1.0.001
+version=1.0.002
 if [ -z "$lidarr_artist_path" ]; then
 	lidarr_artist_path="$1"
 	notfidedBy=Extended_Script
 else
 	notfidedBy=Lidarr
+fi
+
+# auto-clean up log file to reduce space usage
+if [ -f "/config/logs/MetadataPostProcess.txt" ]; then
+	find /config/logs -type f -name "MetadataPostProcess.txt" -size +1024k -delete
 fi
 
 exec &>> "/config/logs/MetadataPostProcess.txt"
@@ -33,6 +38,17 @@ if [ -d "$albumFolder" ]; then
     fi
 else
     log "Processing :: ERROR :: $albumFolder :: folder missing..."
+fi
+
+lrcFile="${lidarr_trackfile_path%.*}.lrc"
+if [ -f "$lrcFile" ]; then
+    rm "$lrcFile"
+fi
+getLyrics="$(ffprobe -loglevel 0 -print_format json -show_format -show_streams "$lidarr_trackfile_path" | jq -r ".format.tags.LYRICS")"
+if [ "$getLyrics" != "null" ]; then
+    log "Processing :: $lidarr_trackfile_path :: Extracting Lyrics..."
+    ffprobe -loglevel 0 -print_format json -show_format -show_streams "$lidarr_trackfile_path" | jq -r ".format.tags.LYRICS" > "$lrcFile"
+    log "Processing :: $lidarr_trackfile_path :: Lyrics extracted to: $lrcFile"
 fi
 
 exit
