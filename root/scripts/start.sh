808fbbcaf45766836e0ab37b934ed4e7e1feaabe
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-07-18 19:31:29 -0400

    Change process order, bug fix

--- root/scripts/start.sh
+++ root/scripts/start.sh
@@ -11,17 +11,17 @@ for (( ; ; )); do
 		chmod 777 "/config/logs" 
 		chown abc:abc "/config/logs" 
 	fi
-	bash /config/extended/scripts/download.sh 2>&1 | tee "/config/logs/extended_script_run_${i}_$(date +"%Y_%m_%d_%I_%M_%p").txt" > /proc/1/fd/1 2>/proc/1/fd/2
 	if [ -f "/config/extended/logs/log-cleanup" ]; then
 		rm "/config/extended/logs/log-cleanup"
 	fi
-	touch -d "8 hours ago" "/config/extended/logs/log-cleanup"
+	touch -d "24 hours ago" "/config/extended/logs/log-cleanup"
 	if find "/config/logs" -type f -iname "extended_script_run_*.txt" -not -newer "/config/extended/logs/log-cleanup" | read; then
-		find "/config/logs" -type f -iname "*.txt" -not -newer "/config/logs/log-cleanup" -delete
+		find "/config/logs" -type f -iname "*.txt" -not -newer "/config/extended/logs/log-cleanup" -delete
 	fi
 	if [ -f "/config/extended/logs/log-cleanup" ]; then
 		rm "/config/extended/logs/log-cleanup"
 	fi
+	bash /config/extended/scripts/download.sh 2>&1 | tee "/config/logs/extended_script_run_${i}_$(date +"%Y_%m_%d_%I_%M_%p").txt" > /proc/1/fd/1 2>/proc/1/fd/2
 	if [ -z "$scriptInterval" ]; then
 		scriptInterval="15m"
 	fi
