88aee7aa08140ecb5241bd4b8e89e458517d9f05
Makario1337 <74052988+Makario1337@users.noreply.github.com>
2022-12-09 18:56:16 +0100

    Adjustment

--- root/scripts/AutoArtistAdder.bash
+++ root/scripts/AutoArtistAdder.bash
@@ -317,34 +317,6 @@ AddTidalArtistToLidarr () {
 	done
 }
 
-LidarrMissingAlbumSearch () {
-
-	log "Begin searching for missing artist albums via Lidarr Indexers..."
-	lidarrArtistIds=$(echo $lidarrMissingAlbumArtistsData | jq -r .id)
-	lidarrArtistIdsCount=$(echo "$lidarrArtistIds" | wc -l)
-	processCount=0
-	for lidarrArtistId in $(echo $lidarrArtistIds); do
-		processCount=$(( $processCount + 1))
-		lidarrArtistData=$(echo $lidarrMissingAlbumArtistsData | jq -r "select(.id==$lidarrArtistId)")
-		lidarrArtistName=$(echo $lidarrArtistData | jq -r .artistName)
-		lidarrArtistMusicbrainzId=$(echo $lidarrArtistData | jq -r .foreignArtistId)
-		if [ -d /config/extended/logs/searched/lidarr/artist ]; then
-			if [ -f /config/extended/logs/searched/lidarr/artist/$lidarrArtistMusicbrainzId ]; then
-				log "$processCount of $lidarrArtistIdsCount :: Previously Notified Lidarr to search for \"$lidarrArtistName\" :: Skipping..."
-				continue
-			fi
-		fi
-		log "$processCount of $lidarrArtistIdsCount :: Notified Lidarr to search for \"$lidarrArtistName\""
-		startLidarrArtistSearch=$(curl -s "$lidarrUrl/api/v1/command" -X POST -H "Content-Type: application/json" -H "X-Api-Key: $lidarrApiKey"  --data-raw "{\"name\":\"ArtistSearch\",\"artistId\":$lidarrArtistId}")
-		if [ ! -d /config/extended/logs/searched/lidarr/artist ]; then
-			mkdir -p /config/extended/logs/searched/lidarr/artist
-			chmod -R 777 /config/extended/logs/searched/lidarr/artist
-		fi
-		touch /config/extended/logs/searched/lidarr/artist/$lidarrArtistMusicbrainzId
-		chmod 777 /config/extended/logs/searched/lidarr/artist/$lidarrArtistMusicbrainzId
-	done
-}
-
 if [ -z $lidarrSearchForMissing ]; then
 	lidarrSearchForMissing=true
 fi
@@ -364,10 +336,6 @@ fi
 if [ "$addRelatedArtists" == "true" ]; then
 	AddDeezerRelatedArtists
 	AddTidalRelatedArtists
-fi
-
-if [ "$addDeezerTopArtists" == "true" ] || [ "$addDeezerTopAlbumArtists" == "true" ] || [ "$addDeezerTopTrackArtists" == "true" ] || [ "$addRelatedArtists" == "true" ]; then
-	LidarrMissingAlbumSearch
 fi
 
 exit
