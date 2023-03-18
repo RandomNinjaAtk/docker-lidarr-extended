aeefa37bd2ec5ac4d9ad9033ec8e8606c7b8651d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-03-18 12:48:36 -0400

    Use universal QueueCleaner

--- root/etc/cont-init.d/98-script-setup.bash
+++ root/etc/cont-init.d/98-script-setup.bash
@@ -34,5 +34,10 @@ find /config/extended -type d -exec chmod 777 {} \;
 chmod -R 777 /config/extended/scripts
 chmod -R 777 /root
 
+
+echo "Setting up scripts..."
+echo "Downloading and setting up QueueCleaner.bash"
+curl "https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/QueueCleaner.bash" -o "/etc/services.d/QueueCleaner.bash"
+chmod 777 "/etc/services.d/QueueCleaner.bash"
 echo "Complete..."
 exit
